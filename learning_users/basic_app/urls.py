ac5e24e95b12eb22202720ee203a279e92005c02
Kivanc Eski <eskikivanc@gmail.com>
2022-01-24 15:03:05 +0300

    initial commit

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,12 @@
+from unicodedata import name
+from django.urls import path
+from basic_app import views
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('register/', views.register, name="register"),
+    path('login/', views.user_login, name="user_login"),
+    path('logout/', views.user_logout, name="user_logout"),
+    path('logout/', views.user_logout, name="logout")
+]
